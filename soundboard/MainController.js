6cb08668694e2e0f337bbf81f8da2662a672cebf
CJP <cjprestia@gmail.com>
2016-04-17 20:27:08 -0400

    Update MainController.js

--- soundboard/MainController.js
+++ soundboard/MainController.js
@@ -1,39 +1,28 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.exercises = [{
-    icon: 'img/pushup.jpg',
-    name: 'Pushups',
-    count: 20
+  $scope.clips = [{
+    icon: 'https://photos-3.dropbox.com/t/2/AAAddaVuCRuWOlSPhDfhkUeTAQb_0YPqgy6AZl8q15tW-g/12/203727679/jpeg/32x32/1/_/1/2/tUP%20FMP%20II.jpg/EKW5x6MEGI4BIAIoAg/buuK_F4VAngPepkpMIrrp8w43canR7Hx7shSf6LrWng?size=800x600&size_mode=3',
+    name: 'Filling My Pants',
   }, {
-    icon: 'img/squat.jpg',
-    name: 'Squats',
-    count: 15
+    icon: 'https://photos-4.dropbox.com/t/2/AAAqLgcp8Vm2_HnLLbHCaC_BRUiakU_PjVngyjwMba02-Q/12/203727679/jpeg/32x32/1/_/1/2/tUP%20Mortal%20KC.jpg/EKW5x6MEGI4BIAIoAg/SllRC2_J__8Mfl0WnaPtHy4oQ10dzTD7L3B_ETxd3q4?size=800x600&size_mode=3',
+    name: 'Mortal K-conflict',
   }, {
-    icon: 'img/pullup.jpg',
-    name: 'Pullups',
-    count: 10
+    icon: 'https://photos-6.dropbox.com/t/2/AACxik6JZAH85Qbn-vruHUpdirbyl_nk5PSS38tEuDmbcg/12/203727679/jpeg/32x32/1/_/1/2/tUP%20PUY%20Brain.jpg/EKW5x6MEGI4BIAIoAg/ut5kZy9ByQFO47VMrJls6eseVSq9QR7MsvVpo2TfaU4?size=800x600&size_mode=3',
+    name: 'Pick Up Your Brain',
   }, {
-    icon: 'img/row.jpg',
-    name: 'Rows',
-    count: 10
+    icon: 'https://photos-5.dropbox.com/t/2/AADsHck-CV7SWVuBitBeaHTEnhbq3aAcjDTLdSaLYgss-A/12/203727679/jpeg/32x32/1/_/1/2/tUP%20PUY%20Mouth.jpg/EKW5x6MEGI4BIAIoAg/6GP-j2ZMzB3CKVYKQQaKaAHtGbfBLCtXlPtamoN7CJc?size=800x600&size_mode=3',
+    name: 'Pick Up Your Mouth',
   }, {
-    icon: 'img/lunge.jpg',
-    name: 'Lunges',
-    count: 10
+    icon: 'https://photos-3.dropbox.com/t/2/AADPZcW-qbDCbKNmxVBQmFFm5d9nMTQcRHpYbcqRDAVN1Q/12/203727679/jpeg/32x32/1/_/1/2/tUP%20Blue%20Beam.jpg/EKW5x6MEGI4BIAIoAg/hCmD_dCXS08Fq012qTSIhyfdxbCeHQXRUHq6Fzama7Y?size=800x600&size_mode=3',
+    name: 'Crispy Critter\'d My Left Foot',
   }, {
-    icon: 'img/stepup.jpg',
-    name: 'Step Ups',
-    count: 10
+    icon: 'https://photos-2.dropbox.com/t/2/AADcXWUA5po-T0Wo4l3hLXbPohkbVYaawYMhk34J3UBGjA/12/203727679/jpeg/32x32/1/_/1/2/tUP%20Something%20Wrong%20Here.jpg/EKW5x6MEGI4BIAIoAg/JXRboCfS3GBw-C_ab9I2HHKNfmRbedzCJuZ0uyhxgv8?size=800x600&size_mode=3',
+    name: 'Now - Something\'s Wrong Here',
   }, {
-    icon: 'img/situp.jpg',
-    name: 'Sit Ups',
-    count: 15
+    icon: 'https://photos-3.dropbox.com/t/2/AAAfCkcYt340npJgubBJClh35BB37QcjFC6_2mXHVlOk7A/12/203727679/jpeg/32x32/1/_/1/2/tUP%20WeDontKnow.JPG/EKW5x6MEGI4BIAIoAg/AXR_OhtXN_0xazkSxwTxg8OufNXAVWVHFUgHDmBm8zU?size=800x600&size_mode=3',
+    name: 'We Don\'t Know',
+  }, {
+    icon: 'https://photos-3.dropbox.com/t/2/AABL2Kar5TDXz0Mbjhl2ERGGKjFizMDdCDwakZZD-ls3gg/12/203727679/jpeg/32x32/1/_/1/2/tUP%20Cactus.jpg/EKW5x6MEGI4BIAIoAg/8rEjucHZAMIFVlSgIlMJXAPcfCLpfK6NnB5BSSp-fj8?size=800x600&size_mode=3',
+    name: 'Japanese Cactus',
   }];
-
-  $scope.decrease = function(index) {
-      $scope.exercises[index].count -= 1;
-    },
-    $scope.increase = function(index) {
-      $scope.exercises[index].count += 1;
-    }
 
 }]);
