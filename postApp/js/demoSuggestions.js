b53845aab755b76b6d7fbbb69082e28c0437af0a
CJP <cjprestia@gmail.com>
2016-05-09 19:25:53 -0400

    Create demoSuggestions.js

---
+++ postApp/js/demoSuggestions.js
@@ -0,0 +1,22 @@
+app.factory('suggestions', [function(){
+	var demoSuggestions = {
+		posts: [
+		{
+			title: 'Focus on love!',
+			upvotes: 34,
+			comments: [],
+		},
+		{
+			title: 'Assume positivity and good intent!',
+			upvotes: 22,
+			comments: [],
+		},
+		{
+			title: 'Be honest with yourself and others!',
+			upvotes: 17,
+			comments: [],
+		}
+		]
+	};
+	return: demoSuggestions;
+}]);
