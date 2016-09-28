185c614b14204043c8bf6606c416003948d5b946
Charles Prestia <cjprestia@gmail.com>
2016-09-28 17:41:43 -0400

    ned to see to audio working online

---
+++ imgoin/assets/js/space.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+
+	// document.getElementById('song').play();
+
+	$('#pausePlay').click(function() {
+		var audioElem = document.getElementById('song');
+		if (audioElem.play) {
+			console.log("pause");
+			audioElem.pause();
+		} else {
+			console.log("play");
+			audioElem.play();
+		}
+	});
+
+	function gifRotate() {
+		console.log("gifRotate");
+		var gifNum = Math.floor(Math.random()*8)+1;
+
+		$('#mainGif').html("<img src='assets/media/A"+gifNum+".gif'>");
+	};
+
+	// function startPlay() {
+	// 	$("#song").next('audio').play();
+	// };
+
+	var interval = window.setInterval(gifRotate(), 15000);
+}
