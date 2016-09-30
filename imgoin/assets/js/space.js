7d0bcceb145e4d32c21a188f295420cc38725483
Charles Prestia <cjprestia@gmail.com>
2016-09-29 23:38:27 -0400

    pausePlay should be gone now

--- imgoin/assets/js/space.js
+++ imgoin/assets/js/space.js
@@ -1,18 +1,5 @@
 window.onload = function() {
 
-	// document.getElementById('song').play();
-
-	$('#pausePlay').click(function() {
-		var audioElem = document.getElementById('song');
-		if (audioElem.play) {
-			console.log("pause");
-			audioElem.pause();
-		} else {
-			console.log("play");
-			audioElem.play();
-		}
-	});
-
 	function gifRotate() {
 		console.log("gifRotate");
 		var gifNum = Math.floor(Math.random()*8)+1;
